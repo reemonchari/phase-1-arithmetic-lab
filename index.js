c2d31b6c45cb60bce24c6ad9547ddff52608108d
+ <+>
2024-09-25 02:26:34 +0300

    Arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+//no 1
+let num1 = 31, num2 = 2
+let multiply
+function multiplication(){
+    multiply = num1 * num2
+    return multiply 
+}
+console.log(multiplication())
 
-// Write your code here
+//no 2
+let random = Math.floor(Math.random() *10) + 1
+
+//no 3
+let num3 = 9, num4 = 5
+let mod 
+function remainder(){
+    mod = num3 % num4
+    return mod
+}
+console.log(remainder())
+
+//no 4
+let max = Math.max(10, 15, 17, 19, 20)
